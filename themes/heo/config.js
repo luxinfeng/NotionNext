8e0b302da5d877a6b097a99ed2008816a0a2f5e2
xinfeng <nimingxiaofeng@sina.com>
2024-06-29 19:18:15 +0800

    修改欢迎词

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -6,7 +6,7 @@ const CONFIG = {
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
     { title: '欢迎来到我的博客', url: 'https://luxinfeng.top' },
-    { title: '欢迎大家随便看看', url: 'https://luxinfeng.top' }
+    { title: '大家随便看看，就当自己家一样，别客气', url: 'https://luxinfeng.top' }
   ],
 
   // 英雄区左右侧组件颠倒位置
